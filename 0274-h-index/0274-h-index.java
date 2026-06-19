11d9dc4d5d51390c24df5ff1f1d1c8e37c523dc9
SAAKSHI SHARMA <saakshi.code@gmail.com>
2026-06-19 12:54:36 +0530

    Time: 0 ms (100%), Space: 43.3 MB (73.09%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,13 +1,28 @@
+
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
 
-        for(int i = 0 ; i<citations.length; i++){
-            int papers = citations.length-i;
-            if(citations[i] >=citations.length-i){
-                 return papers;
+        int n = citations.length;
+        int[] bucket = new int[n + 1];
+
+        for (int c : citations) {
+            if (c >= n) {
+                bucket[n]++;
+            } else {
+                bucket[c]++;
             }
         }
+
+        int papers = 0;
+
+        for (int h = n; h >= 0; h--) {
+            papers += bucket[h];
+
+            if (papers >= h) {
+                return h;
+            }
+        }
+
         return 0;
     }
 }
